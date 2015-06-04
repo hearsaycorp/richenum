dae25fb30598cd4a00831dc5dbeee3316f7c2290
Adam DePue <adepue@hearsaycorp.com>
2015-06-04 17:29:00 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if sys.version_info.major == 2:
 
 setup(
     name='richenum',
-    version='1.0.5',
+    version='1.1.0',
     description='Enum library for python.',
     long_description=(
         open('README.rst').read() + '\n\n' +
